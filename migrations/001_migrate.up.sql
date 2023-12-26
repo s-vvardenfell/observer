ae2cea1ad3783f4e27295e313d529786a6158721
s.vvardenfell <s.vvardenfell@yandex.ru>
2023-12-26 17:09:11 +0300

    upd util, add go.work

--- migrations/001_migrate.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS books (
-    book_id SERIAL PRIMARY KEY NOT NULL,
-    title VARCHAR(100) NOT NULL,
-    author VARCHAR(100)NOT NULL,
-    price FLOAT,
-    description VARCHAR(3001),
-    author_bio VARCHAR(3000)
-);
---
+++ util/go.mod
@@ -0,0 +1,3 @@
+module github.com/s-vvardenfell/observer/util
+
+go 1.20
